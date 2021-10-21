0e0fe0cbfb1856c09f47a298612a790e9d51f99c
omereryilmaz <omer-eryilmaz@hotmail.com>
2021-10-21 16:11:19 +0300

    Project created

---
+++ BookStore.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BookStore.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ BookStore.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BookStore.Services
+{
+    public class Class1
+    {
+    }
+}
